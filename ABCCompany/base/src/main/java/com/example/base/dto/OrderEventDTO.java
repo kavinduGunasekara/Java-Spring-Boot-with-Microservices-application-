f9c9ee5f46e109f14dfa1fa4731fd67c7df099f8
WAKD Gunasekara <137909922+kavinduGunasekara@users.noreply.github.com>
2025-01-08 21:04:02 +0530

    create ordereventDTO
    use to connect invetory and order dtos

---
+++ ABCCompany/base/src/main/java/com/example/base/dto/OrderEventDTO.java
@@ -0,0 +1,12 @@
+package com.example.base.dto;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class OrderEventDTO {
+    private String message;
+    private String status;
+}
